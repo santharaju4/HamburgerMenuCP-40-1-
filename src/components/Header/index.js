973cbb916d73642e7560cd715774c54ffa1cc2e4
santharaju4 <santharaju007@gmail.com>
2022-09-22 10:13:06 +0000

    Hamburger Menu CP-40

---
+++ src/components/Header/index.js
@@ -0,0 +1,65 @@
+import Popup from 'reactjs-popup'
+import {withRouter, Link} from 'react-router-dom'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
+import {AiFillHome} from 'react-icons/ai'
+import {BsInfoCircleFill} from 'react-icons/bs'
+
+import './index.css'
+
+const Header = () => (
+  <div className="nav-header">
+    <div className="nav-content">
+      <Link to="/">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+          alt="website logo"
+          className="website-logo"
+        />
+      </Link>
+
+      <Popup
+        modal
+        trigger={
+          <button
+            type="button"
+            className="hamburger-icon-button"
+            testid="hamburgerIconButton"
+          >
+            <GiHamburgerMenu size="30" />
+          </button>
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              onClick={() => close()}
+              testid="closeButton"
+            >
+              <IoMdClose size="30" color="#616e7c" />
+            </button>
+            <ul className="nav-links-list">
+              <li className="nav-link-item ">
+                <Link to="/" className="nav-link">
+                  <AiFillHome size="36" />
+                  <p className="nav-link-content">Home</p>
+                </Link>
+              </li>
+              <li className="nav-link-item ">
+                <Link to="/about" className="nav-link">
+                  <BsInfoCircleFill size="36" />
+                  <p className="nav-link-content">About</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </Popup>
+    </div>
+  </div>
+)
+
+export default withRouter(Header)
